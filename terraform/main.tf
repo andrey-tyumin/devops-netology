8a7ef25197a56038c38d68120f7b5b9f412bd0a2
imustgetout <posthus@yandex.ru>
2021-08-19 23:57:15 +0300

    add .tf files in terraform directory

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+    region = "us-east-2"
+    }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "72hw" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  hibernation	= true
+  host_id	= "hw72_host"
+  associate_public_ip_address = "true"
+
+  tags = {
+    Name = "HW72"
+  }
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
